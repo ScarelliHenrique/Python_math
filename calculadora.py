f50d27e2b53319cbbfce7738dacb9afa5f2f40a2
EnricoScarelli <106038890+EnricoScarelli@users.noreply.github.com>
2024-06-12 14:50:08 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,4 @@
+#calculadora simples
 calculadora=input('DIGITE A EXPRESSÃO QUE DESEJA: ')
 
 resultado=eval(calculadora) #em Python é usada para avaliar expressões matemáticas ou outras expressões Python como strings
