b33ab624dd7afd3bb7ee9b36daa6baaa899b441a
EnricoScarelli <106038890+EnricoScarelli@users.noreply.github.com>
2024-07-12 10:16:57 -0300

    teste
    teste

---
+++ teste.py
@@ -0,0 +1,12 @@
+#Now look at the code in the editor. The example program isn't very sophisticated, but can you predict its results?
+from math import pi,radians, degrees, sin, cos, tan, asin
+
+ad = 90
+ar = radians(ad)
+ad = degrees(ar)
+
+print(ad == 90.)
+print(ar == pi / 2.)
+print(sin(ar) / cos(ar) == tan(ar))
+print(asin(sin(ar)) == ar)
+    
